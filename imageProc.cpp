94d37e3d35f17d16e31dda5acf87d8bf90ce8663
Alex Kneipp <alexander.h.kneipp@gmail.com>
2016-05-30 15:03:20 -0400

    Corrects incorrect version of code on raspberryPi (I think).

--- imageProc.cpp
+++ imageProc.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
   {
     if(!vcap.read(frame))
       {
-	waitKey();
+            waitKey();
       }
     cvtColor(frame,frame,CV_RGB2HSV);
     imwrite("sourceImage.jpg",frame);
