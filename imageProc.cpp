a0ce2c8e168b5800350f332cd7b6051bac234f68
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2016-06-16 18:57:41 +0000

    Adds timing utilities for testing thresholding speed on different overclock levels

--- imageProc.cpp
+++ imageProc.cpp
@@ -3,26 +3,54 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <sys/time.h>
 
 using namespace cv;
+
+struct timeval system_time;
+
+int vals[1000];
+int count = 0;
 
 int main(int argc, char *argv[])
 {
   VideoCapture vcap(0);//TODO temporary for raspberry pi camera at home, network camera will be implemented later
   Mat frame;
-  //while(true)
+  printf("Image size: %dx%d",frame.rows,frame.cols);
+  while(true)
   {
     if(!vcap.read(frame))
       {
         std::cout<<"Could not read frame!"<<std::endl;
         return -1;
       }
+    gettimeofday(&system_time,NULL);
+    long int beginTime = system_time.tv_sec * 1000 + system_time.tv_usec/1000;
     cvtColor(frame,frame,CV_BGR2RGB);//No idea what the int constant should be for rpicam
-    imwrite("sourceImage.jpg",frame);
+    //imwrite("sourceImage.jpg",frame);
     inRange(frame,Scalar(3,0,0),Scalar(20,255,255),frame);
-    imwrite("ThresholdedImage.jpg",frame);
-    waitKey();
-    // process image
+    //imwrite("ThresholdedImage.jpg",frame);
+    gettimeofday(&system_time,NULL);
+    long int endTime = system_time.tv_sec * 1000 + system_time.tv_usec/1000;
+    printf("Time to process: %d millis\n", endTime - beginTime);
+    if(count <1000)
+      {
+	vals[count]= endTime-beginTime;
+	count++;
+      }
+    else
+      {
+	double average = 0;
+	for(int i = 0; i < count; i++)
+	  {
+	    average += vals[i];
+	  }
+	average /= count + 1;
+	printf("Average processing time: %f millis \nx", average);
+	return 0;
+      }
+    //waitKey();
+    //process image
   }
 
 }
